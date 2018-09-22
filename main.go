e849e78659355f4f60a3697c4b46cad33d558d21
hueyjj <jasperjeng@gmail.com>
2018-09-22 05:05:16 -0700

    Added layouts for search box, sidebar, and result view

--- main.go
+++ main.go
@@ -14,9 +14,16 @@ func main() {
 	}
 	defer g.Close()
 
+	g.Cursor = true
+	g.Mouse = true
+
 	g.SetManagerFunc(layout)
 
 	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.SetKeybinding("search", gocui.KeyEnter, gocui.ModNone, submitQuery); err != nil {
 		log.Panicln(err)
 	}
 
@@ -27,21 +34,39 @@ func main() {
 
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+	if v, err := g.SetView("search", maxX/2-20, 0, maxX/2+20, 2); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(v, "Hello world! Super long text here")
+		v.Editable = true
+		fmt.Fprintln(v, "Search box")
+		if _, err := g.SetCurrentView("search"); err != nil {
+			return err
+		}
 	}
-	if v, err := g.SetView("hello2", maxX/2-7, (maxY/2)+3, maxX/2+14, (maxY/2+2)+3); err != nil {
+
+	if v, err := g.SetView("sidebar", 1, 3, 20, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(v, "Hello world! Super long text here")
+		fmt.Fprintln(v, "Sidebar")
+	}
+
+	if v, err := g.SetView("result", 20, 3, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Result")
 	}
 	return nil
 }
 
 func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
+}
+
+func submitQuery(g *gocui.Gui, v *gocui.View) error {
+	userInput := v.Buffer()
+	fmt.Printf(userInput)
+	return nil
 }
